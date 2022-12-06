727a717b6626dd988660757907fa267a4ddf90ba
Aaron Yankey <aaronyan2001@gmail.com>
2022-12-06 07:41:31 +0000

    adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChartComponent } from './components/chart/chart.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { RatesComponent } from './components/rates/rates.component';
+import { RateComponent } from './components/rate/rate.component';
+import { DetailsComponent } from './components/details/details.component';
+import { MainComponent } from './components/main/main.component';
+import { InputComponent } from './components/input/input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChartComponent,
+    FooterComponent,
+    HeaderComponent,
+    RatesComponent,
+    RateComponent,
+    DetailsComponent,
+    MainComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
