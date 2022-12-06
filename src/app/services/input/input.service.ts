1e6a9f8bc4bcfdff5f3724e581fb76dbda4f07fc
Aaron Yankey <aaronyan2001@gmail.com>
2022-12-06 08:48:30 +0000

    added the input navigator

--- src/app/services/input/input.service.ts
+++ src/app/services/input/input.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InputService {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  gotoDetail(params: any) {
+		  this.router.navigate(['detail'],{ queryParams:params })
+  }
+
+  goHome(params:any) {
+		this.router.navigate([''],{queryParams:params})
+  }
 }
