a67d80cc3f38069a5f4f9908294c7c0dd8751a67
Aaron Yankey <aaronyan2001@gmail.com>
2022-12-06 11:43:30 +0000

    worked on input component css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,24 +11,46 @@ import { RateComponent } from './components/rate/rate.component';
 import { DetailsComponent } from './components/details/details.component';
 import { MainComponent } from './components/main/main.component';
 import { InputComponent } from './components/input/input.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ChartComponent,
-    FooterComponent,
-    HeaderComponent,
-    RatesComponent,
-    RateComponent,
-    DetailsComponent,
-    MainComponent,
-    InputComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+		declarations: [
+				AppComponent,
+				ChartComponent,
+				FooterComponent,
+				HeaderComponent,
+				RatesComponent,
+				RateComponent,
+				DetailsComponent,
+				MainComponent,
+				InputComponent
+		],
+		imports: [
+				BrowserModule,
+				AppRoutingModule,
+				BrowserAnimationsModule,
+
+				MatSelectModule,
+				MatButtonModule,
+				MatFormFieldModule,
+				MatInputModule,
+				MatIconModule,
+				MatCardModule,
+				MatToolbarModule,
+				MatGridListModule,
+				MatProgressBarModule,
+		],
+		providers: [],
+		bootstrap: [AppComponent]
 })
 export class AppModule { }
