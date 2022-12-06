e98b3af399cb912edabfffef9da1fb3ee8538033
Aaron Yankey <aaronyan2001@gmail.com>
2022-12-06 07:58:11 +0000

    added currency type

---
+++ src/app/types/currency.ts
@@ -0,0 +1,40 @@
+export enum Currency {
+		USD = "USD",
+				EUR = "EUR",
+				JPY = "JPY",
+				GBP = "GBP",
+				AUD = "AUD",
+				CAD = "CAD",
+				CHF = "CHF",
+				CNY = "CNY",
+				HKD = "HKD",
+}
+
+
+		export const CurrencyNames = {
+				USD : "United States Dollar",
+				EUR : "European Union Euro",
+				JPY : "Japanese Yen",
+				GBP : "Great British Pound",
+				AUD : "Australian Dollar",
+				CAD : "Canadian Dollar",
+				CHF : "Swiss Franc",
+				CNY : "Chinese Yuan",
+				HKD : "Hong Kong Dollar",
+		}
+
+		export const CURRENCIES = ` ${Currency.USD}, ${Currency.EUR}, ${Currency.JPY}, ${Currency.GBP}, ${Currency.AUD}, ${Currency.CAD}, ${Currency.CHF}, ${Currency.CNY}, ${Currency.HKD}`
+
+		export declare interface IExchange {
+				From: Currency;
+				To: Currency;
+				Amount: Currency;
+		}
+
+		export declare interface IRate {
+				base: Currency;
+				currency:Currency;
+				rate:number;
+				amount?:number;
+		}
+
