880420b8a69062fdd7d247f9fb5ed94a989afa37
Aaron Yankey <aaronyan2001@gmail.com>
2022-12-07 12:44:04 +0000

    ensuring all files are present

---
+++ src/app/services/utility/router.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { RouteReuseStrategy,ActivatedRouteSnapshot, DetachedRouteHandle } from '@angular/router';
+
+@Injectable({
+		providedIn: 'root'
+})
+export class DefaultRouteReuseStrategy implements RouteReuseStrategy {
+		shouldDetach(route: ActivatedRouteSnapshot): boolean { return false; }
+
+		store(route: ActivatedRouteSnapshot, detachedTree: DetachedRouteHandle): void { }
+
+		shouldAttach(route: ActivatedRouteSnapshot): boolean { return false; }
+
+		retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle | null { return null; }
+
+		shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
+				const ret =  future.routeConfig === curr.routeConfig;
+				return ret
+		}
+}
